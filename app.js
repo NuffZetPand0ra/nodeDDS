86c5c6cabd74e294696bd50849131ffcd901d1d4
Nuffy <esbentind@gmail.com>
2016-03-22 00:16:33 +0100

    Now using koad-handlebars for templating

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 var url = require('url');
-var domjs = require('domjs/lib/html5');
 var koa = require('koa');
+var hb = require('koa-handlebars');
+var fs = require('fs');
 var app = koa();
 
 
+app.use(hb({
+  defaultLayout: "main"
+}));
 
-// x-response-time
-
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  this.set('X-Response-Time', ms + 'ms');
-});
-
-// logger
-
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
-  console.log(url.parse(this.url, true, true));
-});
-
-// response
-
-app.use(function *(){
-  this.body = "Hej!";
-//  this.body = domjs.build(mytemplate);
+app.use(function *() {
+  yield this.render("index", {
+    title: "Test Page",
+    name: "World"
+  });
 });
 
 app.listen(8000);
