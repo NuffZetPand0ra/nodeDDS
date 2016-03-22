39fe55746eeedb8cc05e6d868f28d1b7123d4c0e
Nuffy <esbentind@gmail.com>
2016-03-22 05:07:12 +0100

    Added templates for deal and hand, as well as index. Started extracting some cards from the input...

--- app.js
+++ app.js
@@ -1,22 +1,70 @@
 var url = require('url')
   , koa = require('koa')
+  , router = require('koa-router')()
+  , koaBOdy = require('koa-body')
   , hb = require('koa-handlebars')
   , fs = require('fs')
   , app = koa()
+	, dds = require('dds-node-adapter')
 	;
 
 // console.log(hb.options,hb)
 app.use(hb({
   defaultLayout: "main"
 , partialsDir: "partials"
+, helpers: {
+		suitSymbol: function(item){
+			var symbol;
+			switch(item){
+				case 's':
+					symbol = "&spades;"
+					break
+				case 'h':
+					symbol = "&hearts;"
+					break
+				case 'd':
+					symbol = "&diams;"
+					break
+				case 'c':
+					symbol = "&clubs;"
+					break
+			}
+			return symbol
+		},
+		uppercase: function(str){
+			if(typeof str == "string"){
+				return str.toUpperCase()
+			}
+			return str
+		}
+	}
 }));
 
-app.use(function *() {
-  yield this.render("index", {
-    title: "Test Page",
-    name: "World"
-  });
+router.get("/", function *() {
+	if(this.req.url == "/favicon.ico"){return true}
+	var request = url.parse(this.req.url, true)
+	  , data = {
+			title: "DBF DDS"
+		}
+		;
+	console.log("Vi sender!", request, this.request.body)
+  yield this.render("index", data);
 });
+router.get("/solve", function *(){
+	var testData = require('./testhand.json')
+	  , query = url.parse(this.req.url, true)
+		, cardData = query.query.data
+		;
+	var extracted = cardData.match(/[SHRK] ([EKDBT2-9]+)/g)
+	console.log(cardData, extracted)
+	// this.body = JSON.stringify(cardData)
+	
+	// console.log(url.parse(this.req.url, true))
+	// yield this.render("solve", testData)
+	// this.body = "Resultat"
+})
+
+app.use(router.routes())
 
 app.listen(8000);
 console.log(app);
